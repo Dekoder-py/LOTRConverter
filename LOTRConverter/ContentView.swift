9308a3c1a868f39992e73b4bea1a273a4f4c1342
Dekoder-py <kyleyesmariam@icloud.com>
2025-03-27 21:19:04 +1300

    add background, logo and text

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -11,11 +11,21 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             // background image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
             
             VStack {
                 // pony logo
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 
                 // currency exchange text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
                 
                 // conversion section
                 HStack {
