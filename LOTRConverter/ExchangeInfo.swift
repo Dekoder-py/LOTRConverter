ce2202000ed768762182d6fb3d0089dfa1c97c40
Dekoder-py <kyleyesmariam@icloud.com>
2025-03-28 13:28:12 +1300

    Create a new view and add comments to structure view

---
+++ LOTRConverter/ExchangeInfo.swift
@@ -0,0 +1,34 @@
+//
+//  ExchangeInfo.swift
+//  LOTRConverter
+//
+//  Created by Dekoder-py on 3/28/25.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        ZStack {
+            // background
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                // title
+                Text("Exchange Rates")
+                    .font(.largeTitle)
+                
+                // text block
+                
+                // currency * 4
+                
+                // done button
+            }
+        }
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
