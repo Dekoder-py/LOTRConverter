6761920064a525f959dd5b4870230a0238a2cfd3
Dekoder-py <kyleyesmariam@icloud.com>
2025-03-31 07:08:56 +1300

    create enum for all currencyies and add for each loop and v grid

---
+++ LOTRConverter/Currency.swift
@@ -0,0 +1,31 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by Dekoder-py on 3/31/25.
+//
+
+import SwiftUI
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+    
+    var image: ImageResource {
+        switch self {
+        case .copperPenny:
+            .copperpenny
+        case .silverPenny:
+            .silverpenny
+        case .silverPiece:
+            .silverpiece
+        case .goldPenny:
+            .goldpenny
+        case .goldPiece:
+            .goldpiece
+        }
+    }
+}
