47a0d3bbe6c69f2e0174384474ef5676d533aca9
Dekoder-py <kyleyesmariam@icloud.com>
2025-03-30 17:47:57 +1300

    create a select currency view and a currency icon view to use in the select view

---
+++ LOTRConverter/SelectCurrency.swift
@@ -0,0 +1,55 @@
+//
+//  SelectCurrency.swift
+//  LOTRConverter
+//
+//  Created by Dekoder-py on 3/30/25.
+//
+
+import SwiftUI
+
+struct SelectCurrency: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack {
+            // background parchment
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack {
+                // text
+                Text("Select the currency you are starting with:")
+                    .fontWeight(.bold)
+                
+                // currency icons
+                CurrencyIcon(currencyImage: .copperpenny, currencyName: "Copper Penny")
+                
+                
+                // text
+                Text("Select the currency you would like to convert to:")
+                    .fontWeight(.bold)
+                    
+                // currency icons
+                
+                // done button
+                Button("Done") {
+                    dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown.mix(with: .black, by: 0.3))
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+                .padding()
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+            .foregroundStyle(.black)
+        }
+    }
+}
+
+#Preview {
+    SelectCurrency()
+}
